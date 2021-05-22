ab70d00252b57e6aa6b78acf05b648badf0128a7
Abdeljalil El hachimi <ael-hach@e1r6p12.1337.ma>
2021-05-22 21:34:34 +0100

    adds some parsing logic and fixes some errors in curtok_is and peektok_is

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,4 +1,7 @@
 #include "parser.h"
+#include "lexer.h"
+#include "token/token.h"
+#include <stdlib.h>
 
 t_parser parser_new(t_lexer l) {
     t_parser p = {l, {NULL, NULL}, {NULL, NULL}};
@@ -15,7 +18,11 @@ void    next_tok(t_parser *p)
 
 t_program parse_program(t_parser *parser) {
 	t_program program;
-
+	t_command command;
+	program.commands = malloc(sizeof(t_command) + 1);
+	while (strcmp(parser->curr_tok.type, g_eof) != 0) {
+		program.commands = append_command(program.commands, parse_command(parser));
+	}
 	return (program);
 }
 
@@ -25,12 +32,17 @@ t_command parse_command(t_parser *parser) {
 	return (command);
 }
 
+t_command *append_command(t_command *command_list, t_command command) {
+	t_command *new_command_list = malloc(sizeof(t_command));
+	return (new_command_list);
+}
+
 bool curr_tok_is(const t_parser *parser, const char *tok) {
-	return (ft_strncmp(parser->curr_tok.type, tok, ft_strlen(tok)));
+	return (ft_strncmp(parser->curr_tok.type, tok, ft_strlen(tok)) == 0);
 }
 
 bool peek_tok_is(const t_parser *parser, const char *tok) {
-	return (ft_strncmp(parser->peek_tok.type, tok, ft_strlen(tok)));
+	return (ft_strncmp(parser->peek_tok.type, tok, ft_strlen(tok)) == 0);
 }
 
 bool expect_peek(t_parser *parser, const char *tok) {
--- srcs/parser.h
+++ srcs/parser.h
@@ -7,8 +7,12 @@ typedef struct s_parser {
 } t_parser;
 
 typedef struct s_command {
+	const char *command_literal;
     char *args;
     const char *command_type;
+	// built-in
+	// text file
+	// binary
     const char *prev_delim;
     const char *next_delim;
 } t_command;
@@ -23,6 +27,7 @@ t_command parse_command(t_parser *parser);
 bool curr_tok_is(const t_parser *parser, const char *tok);
 bool peek_tok_is(const t_parser *parser, const char *tok);
 bool expect_peek(t_parser *parser, const char *tok);
+t_command *append_command(t_command *command_list, t_command command);
 
 void next_tok(t_parser *p);
 t_parser parser_new(t_lexer l);
