c1d817a71e6204bec7fed4c0aafed9618b9ab0f0
JLLx86 <eabduljalil@outlook.com>
2021-11-15 11:33:00 +0100

    adds basic parsing structing plus removing some dead code

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,102 +1,50 @@
 #include "parser.h"
+#include "strtools/strtools.h"
 #include "lexer.h"
 #include "token/token.h"
 #include <stdlib.h>
 
-t_parser parser_new(t_lexer l) {
-    t_parser p = {l, {NULL, NULL}, {NULL, NULL}};
-    next_tok(&p);
-    next_tok(&p);
+t_parser *parser_new(t_lexer l)
+{
+	t_parser *p = malloc(sizeof(*p));
+	*p = (t_parser){l, {NULL, NULL}, {NULL, NULL}};
+	next_tok(p);
+	next_tok(p);
 	return (p);
 }
 
-void    next_tok(t_parser *p)
+void next_tok(t_parser *p)
 {
-    p->curr_tok = p->peek_tok;
-    p->peek_tok = p->lexer.next_token(&p->lexer);
+	p->curr_tok = p->peek_tok;
+	p->peek_tok = next_token(&p->lexer);
 }
 
-t_program parse_program(t_parser *parser) {
-	t_program program;
-	t_command command;
-	program.commands = malloc(sizeof(t_command) + 1);
-	while (str_match(parser->curr_tok.type, g_eof) != 0) {
-		program.commands = append_command(program.commands, parse_command(parser));
-	}
-	return (program);
-}
-
-bool		str_match(const char *str1, const char *str2) {
-	return (ft_strncmp(str1, str2, ft_strlen(str1)) == 0);
-}
-
-
-t_command	parse_built_in(t_parser *parser) {
+t_command parse_command(t_parser *parser)
+{
 	t_command command;
 
-
-	return command;
-}
-
-t_command	parse_redirection(t_parser *parser) {
-	t_command command;
-
-	
-
-	return command;
-}
-
-t_command	parse_binary(t_parser *parser) {
-	t_command command;
-
-	
-
-	return command;
-}
-
-t_token expand_var(const char *tok_literal) {
-	t_token token;
-	
-	// TO-THINK-ABOUT: TO FREE OR NOT TO FREE????
-	token.literal = getenv(tok_literal);
-	return (token);
-}
-
-t_command parse_command(t_parser *parser) {
-	t_command command;
-
-	if (str_match(parser->curr_tok.type, g_built_in))
-		command = parse_built_in(parser);
-	else if (str_match(parser->curr_tok.type, g_r_redirection))
-		command = parse_redirection(parser);
-	else if (str_match(parser->curr_tok.type, g_arg))
-		command = parse_binary(parser);
-	else if (str_match(parser->curr_tok.type, g_variable)) {
-		parser->curr_tok = expand_var(parser->curr_tok.literal);
-	}
-	else
-		command.type = "error";
 	return (command);
 }
 
-t_command *append_command(t_command *command_list, t_command command) {
-	t_command *new_command_list = malloc(sizeof(t_command));
-	return (new_command_list);
+bool curr_tok_is(const t_parser *parser, const char *tok)
+{
+	return (str_match(parser->curr_tok.type, tok));
 }
 
-bool curr_tok_is(const t_parser *parser, const char *tok) {
-	return (ft_strncmp(parser->curr_tok.type, tok, ft_strlen(tok)) == 0);
+bool peek_tok_is(const t_parser *parser, const char *tok)
+{
+	return (str_match(parser->peek_tok.type, tok));
 }
 
-bool peek_tok_is(const t_parser *parser, const char *tok) {
-	return (ft_strncmp(parser->peek_tok.type, tok, ft_strlen(tok)) == 0);
-}
-
-bool expect_peek(t_parser *parser, const char *tok) {
-	if (peek_tok_is(parser, tok)) {
+bool expect_peek(t_parser *parser, const char *tok)
+{
+	if (peek_tok_is(parser, tok))
+	{
 		next_tok(parser);
 		return (true);
-	} else {
+	}
+	else
+	{
 		return (false);
 	}
 }
--- srcs/parser.h
+++ srcs/parser.h
@@ -1,33 +1,41 @@
 #include "lexer.h"
 
-typedef struct s_parser {
-    t_lexer lexer;
-    t_token curr_tok;
-    t_token peek_tok;
+typedef struct s_parser
+{
+	t_lexer lexer;
+	t_token curr_tok;
+	t_token peek_tok;
 } t_parser;
 
-typedef struct s_command {
-	const char *literal;
-    char *args;
-    const char *type;
-	// built-in
-	// text file
-	// binary
-    const char *prev_delim;
-    const char *next_delim;
+enum io_type
+{
+	FILE,
+	STDOUT,
+	STDIN,
+	STDERR,
+	_PIPE
+};
+
+typedef struct s_io
+{
+	io_type type;
+	char *  value;
+} t_io;
+
+typedef struct s_command
+{
+	char *            command_name;
+	char **           args;
+	t_io              in;
+	t_io              out;
+	struct s_command *next;
 } t_command;
 
-typedef struct s_program {
-    t_command *commands;
-} t_program;
-
-t_program parse_program(t_parser *parser);
 t_command parse_command(t_parser *parser);
 
 bool curr_tok_is(const t_parser *parser, const char *tok);
 bool peek_tok_is(const t_parser *parser, const char *tok);
 bool expect_peek(t_parser *parser, const char *tok);
-t_command *append_command(t_command *command_list, t_command command);
 
-void next_tok(t_parser *p);
-t_parser parser_new(t_lexer l);
+void     next_tok(t_parser *p);
+t_parser *parser_new(t_lexer l);
