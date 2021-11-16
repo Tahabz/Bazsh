92f2747ca8be3a8220bee4ed9577b6c5302c5ae4
Tahabz <etahabaz@gmail.com>
2021-11-16 17:31:52 +0100

    fix bug

--- srcs/executor.c
+++ srcs/executor.c
@@ -7,17 +7,29 @@
 
 t_command init_commands()
 {
+	char **args;
+
+	args = malloc(sizeof(char *));
+
+	args[0] = ft_strdup("lexer.c");
+
+	char **args2;
+
+	args2 = malloc(sizeof(char *));
+
+	args2[0] = ft_strdup("#include");
+
 	t_command command = {
-	    .command_name = "cat",
-	    .args = (char *[1]){"lexer.c"},
-	    .out = (t_io){.type = _PIPE,
+	    .name = "cat",
+	    .args = args,
+	    .out = (t_io){.type = IO_PIPE,
 	                  .value = NULL},
 	};
 
 	t_command *command2 = malloc(sizeof(t_command));
-	command2->command_name = "grep";
-	command2->args = (char *[1]){"#include"},
-	command2->in = (t_io){.type = _PIPE,
+	command2->name = "grep";
+	command2->args = args2,
+	command2->in = (t_io){.type = IO_PIPE,
 	                      .value = NULL};
 
 	command.next = command2;
@@ -30,34 +42,37 @@ int main(int ac, char **av, char **env)
 	t_command command;
 
 	command = init_commands();
-	printf("name = %s, args = %s, in = %d", command.name, command.in);
-	int fd[2];
-	int pid1;
-	int pid2;
-	// char *args[] = {"cat", "lexer.c", NULL};
+	printf("name = %s, args = %s, in = %d\n", command.name, command.args[0], command.in.type);
+	printf("name = %s, args = %s, in = %d", command.next->name, command.next->args[0], command.next->in.type);
 
-	if (pipe(fd) == -1)
-		return -1;
-	pid1 = fork();
-	if (pid1 == 0)
-	{
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execve("/usr/bin/cat", (char *[3]){"cat", "lexer.c", NULL}, env);
-	}
+	return 0;
+	// int fd[2];
+	// int pid1;
+	// int pid2;
+	// // char *args[] = {"cat", "lexer.c", NULL};
 
-	pid2 = fork();
-	if (pid2 == 0)
-	{
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		close(fd[1]);
-		execve("/usr/bin/grep", (char *[3]){"grep", "lexer", NULL}, env);
-	}
+	// if (pipe(fd) == -1)
+	// 	return -1;
+	// pid1 = fork();
+	// if (pid1 == 0)
+	// {
+	// 	dup2(fd[1], STDOUT_FILENO);
+	// 	close(fd[0]);
+	// 	close(fd[1]);
+	// 	execve("/usr/bin/cat", (char *[3]){"cat", "lexer.c", NULL}, env);
+	// }
 
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
+	// pid2 = fork();
+	// if (pid2 == 0)
+	// {
+	// 	dup2(fd[0], STDIN_FILENO);
+	// 	close(fd[0]);
+	// 	close(fd[1]);
+	// 	execve("/usr/bin/grep", (char *[3]){"grep", "lexer", NULL}, env);
+	// }
+
+	// close(fd[0]);
+	// close(fd[1]);
+	// waitpid(pid1, NULL, 0);
+	// waitpid(pid2, NULL, 0);
 }
