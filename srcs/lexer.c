889ff889ade5858f359bd755556f0e7b449580c9
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2021-11-19 18:35:39 +0100

    args are now joined upfront at the lexer insead of the parser

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -1,4 +1,5 @@
 #include "lexer.h"
+#include "strtools/strtools.h"
 #include "token/token.h"
 #include <stddef.h>
 #include <stdio.h>
@@ -19,6 +20,39 @@ t_lexer	new_lexer(const char *input)
 	return (l);
 }
 
+static t_token get_next_arg(t_lexer *lexer)
+{
+	t_token next_tok;
+	if (lexer->ch == '"')
+	{
+		read_char(lexer);
+		next_tok = read_arg_dquotes(lexer);
+	}
+	else if (lexer->ch == '\'')
+	{
+		read_char(lexer);
+		next_tok = read_arg_squotes(lexer);
+	}
+	else if (!is_separator(lexer->ch))
+	{
+		next_tok = read_arg_no_quotes(lexer);
+	}
+	return next_tok;
+}
+
+static t_token join_next_arg(t_lexer *lexer, t_token current_tok)
+{
+	t_token next_tok;
+	const char *temp = current_tok.literal;
+
+	next_tok = get_next_arg(lexer);
+	current_tok.literal = ft_strjoin(current_tok.literal, next_tok.literal);
+	free(next_tok.literal);
+	free((void *)temp);
+	current_tok.type = next_tok.type;
+	return (current_tok);
+}
+
 t_token	read_arg_squotes(t_lexer *lexer)
 {
 	t_token tok;
@@ -29,7 +63,12 @@ t_token	read_arg_squotes(t_lexer *lexer)
 		if (lexer->ch == '\'')
 		{
 			tok.literal = ft_substr(lexer->input, position, lexer->position - position);
-			tok.type = SINGLE_Q_ARG;
+			tok.type = ARG;
+			if (peek_char(lexer) == '\"' || peek_char(lexer) == '\'' || !is_separator(peek_char(lexer)))
+			{
+				read_char(lexer);
+				tok = join_next_arg(lexer, tok);
+			}
 			return (tok);
 		}
 		read_char(lexer);
@@ -42,6 +81,7 @@ t_token	read_arg_squotes(t_lexer *lexer)
 t_token	read_arg_dquotes(t_lexer *lexer)
 {
 	t_token tok;
+	t_token temp_tok;
 	const int position = lexer->position;
 
 	while (lexer->ch != '\0')
@@ -49,7 +89,13 @@ t_token	read_arg_dquotes(t_lexer *lexer)
 		if (lexer->ch == '\"')
 		{
 			tok.literal = ft_substr(lexer->input, position, lexer->position - position);
-			tok.type = DOUBLE_Q_ARG;
+			tok.type = ARG;
+			// TODO expand tok.literal variables
+			if (peek_char(lexer) == '\"' || peek_char(lexer) == '\'' || !is_separator(peek_char(lexer)))
+			{
+				read_char(lexer);
+				tok = join_next_arg(lexer, tok);
+			}
 			return (tok);
 		}
 		read_char(lexer);
@@ -72,6 +118,10 @@ t_token	read_arg_no_quotes(t_lexer *lexer)
 	}
 	tok.literal = ft_substr(lexer->input, position, lexer->position - position);
 	tok.type = ARG;
+	if (lexer->ch == '\"' || lexer->ch == '\'')
+	{
+		tok = join_next_arg(lexer, tok);
+	}
 	return (tok);
 }
 
