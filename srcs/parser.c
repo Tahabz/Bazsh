0fbd83ea668bb8452f7076df12b43c41c2a68e47
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2021-12-04 19:05:53 +0100

    add parsing logic for both args and pipes

--- srcs/parser.c
+++ srcs/parser.c
@@ -9,7 +9,7 @@
 t_parser *parser_new(t_lexer l)
 {
 	t_parser *p = malloc(sizeof(*p));
-	*p = (t_parser){l, {NULL, NULL}, {NULL, NULL}};
+	*p = (t_parser){l, {NULL, NULL}, {NULL, NULL}, NULL};
 	next_tok(p);
 	next_tok(p);
 	return (p);
@@ -23,14 +23,29 @@ void next_tok(t_parser *p)
 	p->peek_tok = next_token(&p->lexer);
 }
 
-void append_arg(t_arg *current_arg, t_parser *p)
+t_arg *new_arg(const char *value)
 {
-	current_arg = current_arg->next;
-	while (current_arg)
-		current_arg = current_arg->next;
-	current_arg = malloc(sizeof(*current_arg));
-	current_arg->val = ft_strdup(p->curr_tok.literal);
-	current_arg->next = NULL;
+	t_arg *arg = malloc(sizeof(*arg));
+	arg->val = ft_strdup(value);
+	arg->next = NULL;
+	return arg;
+}
+
+void append_arg(t_arg **head_arg, const char *value)
+{
+	t_arg *tail;
+
+	tail = *head_arg;
+	if (*head_arg == NULL)
+	{
+		*head_arg = new_arg(value);
+		return ;
+	}
+	while (tail->next != NULL)
+	{
+		tail = tail->next;
+	}
+	tail->next = new_arg(value);
 }
 
 t_command *create_command()
@@ -39,7 +54,7 @@ t_command *create_command()
 
 	command = malloc(sizeof(*command));
 	*command = (t_command){
-	    .arg = {NULL, NULL},
+	    .arg = NULL,
 	    .in = {IO_STDIN, NULL},
 	    .out = {IO_STDOUT, NULL},
 	    .next = NULL,
@@ -63,74 +78,46 @@ void raise_syntax_error(const char *expected, const char *found)
 	      expected, found);
 }
 
+void parse_arg(t_parser *parser, t_command *cmd)
+{
+	append_arg(&cmd->arg, parser->curr_tok.literal);
+	if (peek_tok_is(parser, ARG))
+		parser->parsing_state = parse_arg;
+	else if (peek_tok_is(parser, PIPE))
+		parser->parsing_state = parse_pipe;
+	else
+		parser->parsing_state = NULL;
+}
+
+void parse_pipe(t_parser *parser, t_command *cmd)
+{
+	cmd->out.type = IO_PIPE;
+	parser->parsing_state = NULL;
+}
+
 t_command *parse_command(t_parser *parser)
 {
 	t_command *command;
 
 	command = create_command();
 	if (curr_tok_is(parser, ARG))
+		parser->parsing_state = parse_arg;
+	else
+		return NULL;
+	while (parser->parsing_state)
 	{
-		command->arg.val = ft_strdup(parser->curr_tok.literal);
-		next_tok(parser);
-		while (curr_tok_is(parser, ARG))
-		{
-			append_arg(&command->arg, parser);
-			next_tok(parser);
-		}
-		if (curr_tok_is(parser, PIPE))
-		{
-			command->out.type = IO_PIPE;
-			return command;
-		}
-		else if (curr_tok_is(parser, R_REDIRECTION))
-		{
-			command->out.type = IO_FILE;
-			next_tok(parser);
-			if (curr_tok_is(parser, ARG))
-				command->out.value = ft_strdup(parser->curr_tok.literal);
-			else
-				raise_syntax_error(ARG, parser->curr_tok.type);
-		}
-		else if (curr_tok_is(parser, L_REDIRECTION))
-		{
-			command->in.type = IO_FILE;
-			next_tok(parser);
-			if (curr_tok_is(parser, ARG))
-				command->in.value = ft_strdup(parser->curr_tok.literal);
-			else
-				raise_syntax_error(ARG, parser->curr_tok.type);
-		}
-		else if (curr_tok_is(parser, APPEND))
-		{
-			command->out.type = IO_FILE_APPEND;
-			next_tok(parser);
-			if (curr_tok_is(parser, ARG))
-				command->out.value = ft_strdup(parser->curr_tok.literal);
-			else
-				raise_syntax_error(ARG, parser->curr_tok.type);
-		}
-		else if (curr_tok_is(parser, HEREDOC))
-		{
-			command->out.type = IO_FILE_APPEND;
-			next_tok(parser);
-			if (curr_tok_is(parser, ARG))
-				command->out.value = ft_strdup(parser->curr_tok.literal);
-			else
-				raise_syntax_error(ARG, parser->curr_tok.type);
-		}
+		parser->parsing_state(parser, command);
 		next_tok(parser);
 	}
-	else
-		return (NULL);
 	return (command);
 }
 
 t_command *start_parser(t_parser *parser)
 {
-	t_command *command;
-	t_command *current = command;
+	const t_command *command = parse_command(parser);
+	t_command *current = (t_command *)command;
 
-	while ((current = parse_command(parser)))
+	while ((current->next = parse_command(parser)))
 	{
 		current = current->next;
 	}
--- srcs/parser.h
+++ srcs/parser.h
@@ -1,10 +1,15 @@
 #include "lexer.h"
 
+typedef struct s_parser t_parser;
+typedef struct s_command t_command;
+typedef void t_parsing_state(t_parser *parser, t_command *cmd);
+
 typedef struct s_parser
 {
 	t_lexer lexer;
 	t_token curr_tok;
 	t_token peek_tok;
+	t_parsing_state *parsing_state;
 } t_parser;
 
 enum io_type
@@ -32,7 +37,7 @@ typedef struct s_arg
 
 typedef struct s_command
 {
-	t_arg             arg;
+	t_arg             *arg;
 	t_io              in;
 	t_io              out;
 	struct s_command *next;
@@ -44,6 +49,8 @@ t_command *start_parser(t_parser *parser);
 bool curr_tok_is(const t_parser *parser, const char *tok);
 bool peek_tok_is(const t_parser *parser, const char *tok);
 bool expect_peek(t_parser *parser, const char *tok);
+void parse_arg(t_parser *parser, t_command *cmd);
+void parse_pipe(t_parser *parser, t_command *cmd);
 
 void      next_tok(t_parser *p);
 t_parser *parser_new(t_lexer l);
