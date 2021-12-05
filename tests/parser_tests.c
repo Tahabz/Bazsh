823a881ead0450e9ad6094cd7b8d9190bd4294cc
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2021-12-05 16:13:00 +0100

    update parser test

--- tests/parser_tests.c
+++ tests/parser_tests.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	const t_lexer lexer = new_lexer("echo hello | cat | hello > test.c");
+	const t_lexer lexer = new_lexer("echo hello | cat | hello > test.c >> test2.c > test3.c < inputfile inputfile2 inputfile3");
 	t_parser *parser = parser_new(lexer);
 	t_command  *command = start_parser(parser);
 	puts(command->arg->val);
