0cd9cc405f4f52033bd79669e3977905019b0b85
Tahabz <etahabaz@gmail.com>
2021-12-11 12:27:27 +0100

    handle multiple pipes

--- tests/parser_tests.c
+++ tests/parser_tests.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	const t_lexer lexer = new_lexer("/bin/cat srcs/executor.c | grep main");
+	const t_lexer lexer = new_lexer("/bin/cat srcs/executor.c | /usr/bin/grep main | /usr/bin/wc -l");
 	t_parser *    parser = parser_new(lexer);
 	t_command *   command = start_parser(parser);
 	// puts(command->arg->val);
