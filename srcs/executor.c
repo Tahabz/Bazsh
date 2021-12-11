916f8d5e1ae1fd5f109506fc4aac197b7833d9ba
Tahabz <etahabaz@gmail.com>
2021-12-11 15:21:07 +0100

    extract functions from the pipe handling loop

--- srcs/executor.c
+++ srcs/executor.c
@@ -5,80 +5,75 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+int get_args_count(t_arg *arg)
+{
+	int i;
+
+	i = 0;
+	while (arg)
+	{
+		i++;
+		arg = arg->next;
+	}
+	return (i);
+}
+
+char **args_to_arr(t_arg *arg)
+{
+	int    args_count;
+	char **args;
+	int    i;
+
+	args_count = get_args_count(arg);
+	args = (char **) malloc((args_count + 1) * sizeof(char *));
+	i = 0;
+	while (arg)
+	{
+		args[i] = arg->val;
+		arg = arg->next;
+		i++;
+	}
+	args[i] = NULL;
+	return (args);
+}
+
+void exec_command(t_command *command, char **env)
+{
+	char **command_args = args_to_arr(command->arg);
+	if (execve(command->arg->val, command_args, env) == -1)
+	{
+		perror("execve");
+		exit(1);
+	}
+	free(command_args);
+}
+
 int main(int ac, char **av, char **env)
 {
-	const t_lexer lexer = new_lexer("/bin/cat srcs/executor.c | /usr/bin/grep main");
+	const t_lexer lexer = new_lexer("/bin/cat srcs/executor.c | /usr/bin/grep main | /usr/bin/wc -l");
 	t_parser *    parser = parser_new(lexer);
 	t_command *   command = start_parser(parser);
 
 	int fd[2];
-	int fdx[2];
-	int pid1;
-	int pid2;
-	int pidx;
+	int pid;
+	int tfd;
 
-	int arr[2];
-	if (pipe(fdx) == -1)
-		return -1;
-	int i = 0;
 	while (command)
 	{
-		pidx = fork();
-		arr[i] = pidx;
-		i++;
-		if (pidx == 0)
+		if (pipe(fd) == -1)
+			return -1;
+		pid = fork();
+		if (pid == 0)
 		{
-			if (command->out_sequence && command->out_sequence->type == IO_PIPE)
-			{
-				dup2(fdx[1], STDOUT_FILENO);
-			}
-			if (!command->in_sequence && !command->out_sequence)
-			{
-				dup2(fdx[0], STDIN_FILENO);
-			}
-			close(fdx[1]);
-			close(fdx[0]);
-			if (execve(command->arg->val, (char *[3]){command->arg->val, command->arg->next->val, NULL}, env) == -1)
-			{
-				perror("execve");
-				exit(1);
-			}
+			dup2(tfd, STDIN_FILENO);
+			if (command->next)
+				dup2(fd[1], STDOUT_FILENO);
+			close(fd[0]);
+			exec_command(command, env);
 		}
-
+		wait(NULL);
+		close(fd[1]);
+		tfd = fd[0];
 		command = command->next;
 	}
-	close(fdx[1]);
-	close(fdx[0]);
-	waitpid(arr[0], NULL, 0);
-	waitpid(arr[1], NULL, 0);
-	// if (pipe(fd) == -1)
-	// 	return -1;
-	// pid1 = fork();
-	// if (pid1 == 0)
-	// {
-	// 	dup2(fd[1], STDOUT_FILENO);
-	// 	close(fd[0]);
-	// 	close(fd[1]);
-	// 	execve("/bin/cat", (char *[3]){"cat", "srcs/executor.c", NULL}, env);
-	// }
-
-	// pid2 = fork();
-	// if (pid2 == 0)
-	// {
-	// 	dup2(fd[0], STDIN_FILENO);
-	// 	close(fd[0]);
-	// 	close(fd[1]);
-	// 	execve("/usr/bin/grep", (char *[3]){"grep", "main", NULL}, env);
-	// }
-
-	// pid2 = fork();
-	// if (pid2 == 0)
-	// {
-
-	// }
-
-	close(fdx[0]);
-	close(fdx[1]);
-	waitpid(pid1, NULL, 0);
-	waitpid(pid2, NULL, 0);
 }
