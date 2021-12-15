590aff37d27b7fea682c39ae2bf3c9e0f7518836
Tahabz <etahabaz@gmail.com>
2021-12-15 09:54:44 +0100

    fix pipes bugs (now working asynchronously)

--- srcs/executor.c
+++ srcs/executor.c
@@ -37,6 +37,35 @@ char **args_to_arr(t_arg *arg)
 	return (args);
 }
 
+char *join_path(char *str1, char *str2)
+{
+	char *tmp_path = ft_strjoin(str1, "/");
+	char *joined_path = ft_strjoin(tmp_path, str2);
+	free(tmp_path);
+	return (joined_path);
+}
+
+char *get_command_path(char *command_name)
+{
+	char **paths;
+	int    i;
+	char  *command_path;
+
+	command_path = NULL;
+	char *x = getenv("PATH");
+	printf("PATH IS = %s", x);
+	paths = ft_split(x, ':');
+	i = 0;
+	while (paths[i])
+	{
+		command_path = join_path(paths[i], command_name);
+		if (access(command_path, F_OK) == 0)
+			return (command_path);
+		i++;
+	}
+	return (command_path);
+}
+
 void exec_command(t_command *command, char **env)
 {
 	char **command_args = args_to_arr(command->arg);
@@ -48,32 +77,70 @@ void exec_command(t_command *command, char **env)
 	free(command_args);
 }
 
+void closepipes(int *pipes[2])
+{
+	int i = 100;
+	while (i)
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+		i--;
+	}
+}
+
 int main(int ac, char **av, char **env)
 {
 	const t_lexer lexer = new_lexer("/bin/cat srcs/executor.c | /usr/bin/grep main | /usr/bin/wc -l");
-	t_parser *    parser = parser_new(lexer);
-	t_command *   command = start_parser(parser);
+	t_parser     *parser = parser_new(lexer);
+	t_command    *command = start_parser(parser);
 
-	int fd[2];
-	int pid;
-	int tfd;
+	// int fd[1024][2];
+	pid_t pid;
+	int   old_fd[2];
+	int   new_fd[2];
+	int   i;
 
+	i = 0;
 	while (command)
 	{
-		if (pipe(fd) == -1)
-			return -1;
+		if (command->next)
+			pipe(new_fd);
 		pid = fork();
 		if (pid == 0)
 		{
-			dup2(tfd, STDIN_FILENO);
+			if (i > 0)
+			{
+				dup2(old_fd[0], STDIN_FILENO);
+				close(old_fd[0]);
+				close(old_fd[1]);
+			}
 			if (command->next)
-				dup2(fd[1], STDOUT_FILENO);
-			close(fd[0]);
+			{
+				close(new_fd[0]);
+				dup2(new_fd[1], STDOUT_FILENO);
+				close(new_fd[1]);
+			}
 			exec_command(command, env);
 		}
-		wait(NULL);
-		close(fd[1]);
-		tfd = fd[0];
+		if (i > 0)
+		{
+			close(old_fd[0]);
+			close(old_fd[1]);
+		}
+		if (command->next)
+		{
+			old_fd[0] = new_fd[0];
+			old_fd[1] = new_fd[1];
+		}
+		i++;
 		command = command->next;
 	}
+
+	if (i > 1)
+	{
+		close(old_fd[0]);
+		close(old_fd[1]);
+	}
+
+	return 0;
 }
