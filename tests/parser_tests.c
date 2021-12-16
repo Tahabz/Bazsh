26da75d527e63874221228c5a468f72c9da71ed3
JLL32 <eabduljalil@outlook.com>
2021-12-16 17:15:04 +0100

    support in/out redirection, append, heredoc at the beginning of the command

--- tests/parser_tests.c
+++ tests/parser_tests.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	const t_lexer lexer = new_lexer("echo hello |\t cat | hello > test.c arg2 >> test2.c > test3.c < inputfile << delim < inputfile2 arg3 << delim2");
+	const t_lexer lexer = new_lexer("<file1 echo hello |\t cat | hello > test.c arg2 >> test2.c > test3.c < inputfile << delim < inputfile2 arg3 << delim2");
 	t_parser *parser = parser_new(lexer);
 	t_command  *command = start_parser(parser);
 	puts(command->arg->val);
