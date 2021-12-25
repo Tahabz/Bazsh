35dac07644849e42ca345bdfe2eaa82ebb3816c5
Tahabz <etahabaz@gmail.com>
2021-12-25 13:06:08 +0100

    add exit and free memory

--- srcs/executor.h
+++ srcs/executor.h
@@ -1,27 +1,29 @@
 #include "../srcs/parser.h"
 
+int code = 0;
+
 typedef struct s_executor
 {
 	int        new_fd[2];
 	int        old_fd[2];
 	int        command_position;
 	int        pids[1000];
-	char      *commands_paths[1000];
-	char     **commands_args[1000];
+	char *     commands_paths[1000];
+	char **    commands_args[1000];
 	t_command *command;
-	char    ***env;
+	char ***   env;
 } t_executor;
 
 typedef struct s_parent_command
 {
 	bool is_parent_command;
-	void (*handler)(char **, char ***);
+	void (*handler)(t_arg *, char ***);
 } t_parent_command;
 
 typedef struct t_child_command
 {
 	bool is_child_command;
-	void (*handler)(char **, char **);
+	void (*handler)(t_arg *, char **);
 } t_child_command;
 typedef struct s_file
 {
