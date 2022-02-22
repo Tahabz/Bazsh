af26fc87843bbf9145c7614b1d982653bb5c78bf
Tahabz <mtahabaz@gmail.com>
2022-02-22 13:51:26 +0100

    add read_and_execute

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -13,21 +13,14 @@ void start_execution(t_executor *executor, char **env)
 	}
 }
 
-int main(int ac, char **av, char **env)
+void read_and_execute(t_executor *executor, char **env)
 {
-	char      *cmd;
-	t_lexer    lexer;
-	t_parser  *parser;
-	t_executor executor;
-	int        i = 0;
+	char     *cmd;
+	t_lexer   lexer;
+	t_parser *parser;
 
-	g_status = 0;
-	executor.env = (char ***) malloc(sizeof(char **));
-	*executor.env = copy_env(env);
-	init_signals();
 	while (true)
 	{
-		executor.command_position = 0;
 		g_forked = false;
 		cmd = readline("bazsh$ ");
 		if (cmd)
@@ -35,14 +28,25 @@ int main(int ac, char **av, char **env)
 			add_history(cmd);
 			lexer = new_lexer(cmd);
 			parser = parser_new(lexer);
-			executor.command = start_parser(parser);
-			start_execution(&executor, env);
-			free_all_memory(executor, parser);
-			waitpids(executor.pids, executor.command_position);
+			executor->command = start_parser(parser);
+			start_execution(executor, env);
+			free_all_memory(*executor, parser);
+			waitpids(executor->pids, executor->command_position);
 		}
 		else
 			exit(atoi(g_code));
 	}
+}
+
+int main(int ac, char **av, char **env)
+{
+	t_executor executor;
+
+	g_status = 0;
+	executor.env = (char ***) malloc(sizeof(char **));
+	*executor.env = copy_env(env);
+	init_signals();
+	read_and_execute(&executor, env);
 	free_double_pointer(*executor.env);
 	return (0);
 }
